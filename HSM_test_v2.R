9d58b1e90fe69eee77a4839c8bb216652c33532f
juanzuloaga <jzuloaga65@gmail.com>
2022-04-22 10:06:51 -0400

    Added predictors Removing colinear predictors Fixed similar extent
    Markdown template

--- HSM_test_v2.R
+++ HSM_test_v2.R
@@ -39,22 +39,22 @@ lapply(list.of.packages, library, character.only =TRUE)
 
 # 1.3. Creating a directory to save temporary files that will be deleted after process
   
-    if(!dir.exists("./temp_HDMs_to_REMOVE")){
-      dir.create("./temp_HDMs_to_REMOVE")
+    if(!dir.exists("./temp_HMs_to_REMOVE")){
+      dir.create("./temp_HMs_to_REMOVE")
       print("dir created")
     }else{
       print("dir exists")
     }
 
 # 1.4. Setting temp directory
-  rasterOptions(tmpdir=file.path("./temp_SDMs_to_REMOVE"))
+  rasterOptions(tmpdir=file.path("./temp_HMs_to_REMOVE"))
 
   
 # 1.5. Setting Projection to preserve area for all files 
   
   # Lon-Lat 
-    wgs84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "
-  
+   # wgs84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "
+    wgs84 <- "+proj=longlat +datum=WGS84 +no_defs"
   # Albers Equal Area Conic
   #[See:](https://spatialreference.org/ref/esri/102001/proj4/) 
     aeac="+proj=aea +lat_1=50 +lat_2=70 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
@@ -63,7 +63,7 @@ lapply(list.of.packages, library, character.only =TRUE)
 
 
 # 2. Species -----------
-myspecies <- c("Nestotus macleanii")
+  myspecies <- c("Nestotus macleanii")
 
 
 # 3.Occurrences ------------
@@ -82,15 +82,8 @@ myspecies <- c("Nestotus macleanii")
                                               # GBIF has a maximum of 100,000.
                                               # If the species has more than 100,000 occ we need to ask GBIF for a zipfile
 
- 
-# 3.3. Loading other data sets
-  # e.g., iNaturalist (however GBIF has iNaturalist records) and eBird
-  
-  
-# 3.4. Joining data sets
-  
-  
-# 3.5. Creating spatial features (points)
+
+# 3.4. Creating spatial features (points)
   
   Obs_gbif_data_sp <- data.frame(cbind(Obs_gbif_data$data$decimalLongitude, Obs_gbif_data$data$decimalLatitude))%>%
     sp::SpatialPoints(proj4string=CRS(wgs84))
@@ -98,6 +91,9 @@ myspecies <- c("Nestotus macleanii")
   # create data.frame
   Obs_gbif_data_df <- data.frame(Obs_gbif_data_sp)
   
+  # Transform for plotting
+  Obs_gbif_data_sf <- st_as_sf(Obs_gbif_data_sp)%>%
+    st_transform(aeac)
   
 # 4. Area of study -------------
   
@@ -106,68 +102,154 @@ myspecies <- c("Nestotus macleanii")
   # Several options here:
     # Geographic range: not for many species
     # Ecoregions
-    # Arbitray polygon (Ontario, Canada, etc)
-    # Minumum convex polygon with buffer
+    # ArbitraRy polygon (Ontario, Canada, etc)
+    # MinImum convex polygon with buffer
   
-  # Minumum convex polygon (mcp) with buffer  
+  # MinImum convex polygon (mcp) with buffer  
   
     box_extent_analysis <- mcp(Obs_gbif_data_sp, percent = 100)%>%
      st_as_sf()%>%
       st_transform(crs=aeac)
     
     # Buffering box extent
-      box_extent_analysis_bf <-  st_buffer(box_extent_analysis, dist =  100000)
+      box_extent_analysis_bf <-  st_buffer(box_extent_analysis, dist =  100000)%>%
+        st_transform(crs=wgs84)
 
-  
+    # Tranform for plotting
+      box_extent_analysis_bf_aeac <- st_as_sf(box_extent_analysis_bf)%>%
+        st_transform(aeac)
 
-
+     
 
 # 5. Predictors ------------
 
 ### Let's start with WORLDCLIM (we are going to use CHELSA)
 
 
-# 5.1 Bioclim
-
+# 5.2. Topographic heterogeneity
+  
+  # Vector_Ruggedness_Measure
+  vrm <- raster("./Data/Topographic_Index/vrm_1KMmn_GMTEDmd.tif") %>%
+    crop(box_extent_analysis_bf) 
+  names(vrm) <- "Vector_Ruggedness_Measure"
+  
+  # Roughness
+  roughness <- raster("./Data/Topographic_Index/roughness_1KMmn_GMTEDmd.tif") %>%
+    crop(box_extent_analysis_bf)   %>%
+    resample(vrm, method = "ngb")
+  names(roughness) <- "Roughness"
+  
+  # Slope
+  Slope <- raster("./Data/Topographic_Index/slope_1KMmn_GMTEDmd.tif") %>%
+    crop(box_extent_analysis_bf)  %>%
+    resample(vrm, method = "ngb")
+  names(Slope) <- "Slope"
+  
+  
+  # Eastness
+  Eastness <- raster("./Data/Topographic_Index/eastness_1KMmn_GMTEDmd.tif") %>%
+    crop(box_extent_analysis_bf)  %>%
+    resample(vrm, method = "ngb")
+  names(Eastness) <- "Eastness"
+  
+  # Northness
+  Northness <- raster("./Data/Topographic_Index/northness_1KMmn_GMTEDmd.tif") %>%
+    crop(box_extent_analysis_bf)  %>%
+    resample(vrm, method = "ngb")
+  names(Northness) <- "Northness"
+  
+  
+# 5.2. Bioclim
+  
   # Idenfitying tiles based on Area of study
-      
-    box_extent_bioclim <- tile_name(as(box_extent_analysis_bf, 'Spatial'), "worldclim") # determine which WorldClim tiles your study area intersects with.
-    
+  
+  box_extent_bioclim <- tile_name(as(box_extent_analysis_bf, 'Spatial'), "worldclim") # determine which WorldClim tiles your study area intersects with.
+  
   # Creating folder
-    if(dir.exists("./bioclim_t")){
-    }else{
-      out_bioclim <-   dir.create("./bioclim_t")}
-    
+  if(dir.exists("./bioclim_t")){
+  }else{
+    out_bioclim <-   dir.create("./bioclim_t")}
+  
   # Loading tiles
-    clim_tiles <- tile_get(box_extent_bioclim, name =  "worldclim", var="bio", path = "./bioclim_t") # for 0.5 arcmin worldclim tiles of 
-    
+  clim_tiles <- tile_get(box_extent_bioclim, name =  "worldclim", var="bio", path = "./bioclim_t") # for 0.5 arcmin worldclim tiles of 
+  
   # Merging tiles
-    clim_tiles_merge <- tile_merge(clim_tiles)
-
-
-
-# Agregagate (Let's try coarse resolution to speed up the process)!!!!!!!!!!!!!!!!
-  clim_tiles_merge_agg <- raster::aggregate(clim_tiles_merge, fact=10, fun=mean)
-
-
-# 5.2. Topogrephic heterogeneity
+  clim_tiles_merge <- tile_merge(clim_tiles)
+  
+  
+  
+  # Agregagate (Let's try coarse resolution to speed up the process)!!!!!!!!!!!!!!!!
+  clim_tiles_merge_agg <- raster::aggregate(clim_tiles_merge, fact=10, fun=mean) %>%
+    resample(vrm, method = "ngb")
+  
   
   
 # 5.3. Productivity
   
+  # Loading Dynamic Habitat Index  (all .tiff files)
+    fpar_all <- list.files("./Data/fpar_can", pattern=".tif$", full.names = T)
   
+  #  Band 1
+      fpar_b1_mean <- lapply(fpar_all, raster, band=1) %>%           # Create raster
+        stack()  %>%                                                 # Stack them
+        crop(box_extent_analysis_bf)%>%                              # Crop to extent
+        calc(mean)%>%                                                # Apply function to calculate mean
+        resample(vrm, method = "ngb")                                # resample using previous raster
+      names(fpar_b1_mean) <- "Cummulative_annual_productivity_b1"    # rename variable
+  
+  
+  #  Band 2
+      fpar_b2_mean <- lapply(fpar_all, raster, band=2) %>%           # Create raster
+        stack()  %>%                                                 # Stack them
+        crop(box_extent_analysis_bf)%>%                              # Crop to extent
+        calc(mean)%>%                                                # Apply function to calculate mean
+        resample(vrm, method = "ngb")                                # resample using previous raster
+      names(fpar_b2_mean) <- "Minimum_annual_productivity_b2"        # rename variable
+          
+      
+  #  Band 3
+      fpar_b3_mean <- lapply(fpar_all, raster, band=3) %>%           # Create raster
+        stack()  %>%                                                 # Stack them
+        crop(box_extent_analysis_bf)%>%                              # Crop to extent
+        calc(mean)%>%                                                # Apply function to calculate mean
+        resample(vrm, method = "ngb")                                # resample using previous raster
+      names(fpar_b3_mean) <- "Variation_annual_productivity_b3"      # rename variable
+
+      
 # 5.4. Water proximity
-  
-  
+      
+  # Percentage of lakes
+      
+    lakes_100m <- raster("./Data/Lakes/Lakes_100_wgs84.tif")%>% # Lakes North America 100m resolution
+      crop(box_extent_analysis_bf)
+    lakes_100m_agg <- aggregate(lakes_100m, fact=10, fun=sum)%>% # aggregating 100m cells into 1000m cells, using 'sum' function
+      resample(vrm, method = "ngb")
+    names(lakes_100m_agg) <- "Lakes_percentage"
+    
+
+# 5.5  Stacking Predictors ----------
+    
+    predictors <- stack(vrm,
+                   roughness,
+                   Slope,
+                   Eastness,
+                   Northness,
+                   lakes_100m_agg,
+                   if(maxValue(fpar_b1_mean) != 0 && minValue(fpar_b1_mean) !=0){fpar_b1_mean}else{},
+                   if(maxValue(fpar_b2_mean) != 0 && minValue(fpar_b2_mean) !=0){fpar_b2_mean}else{},
+                   if(maxValue(fpar_b3_mean) != 0 && minValue(fpar_b3_mean) !=0){fpar_b3_mean}else{},
+                   clim_tiles_merge_agg, na.rm=T)
+    
+    
 # 5.5. Removing collinear variables
 
 # Calculating colinearity
-    clim_collinearity_test <- removeCollinearity(clim_tiles_merge_agg,
+    collinearity_test <- removeCollinearity(predictors,
                                              multicollinearity.cutoff = 0.70, 
                                              plot = F, select.variables = T, sample.points = FALSE)
 
   # Subsetting variables
-    clim_noncollinear <- stack(subset(clim_tiles_merge_agg, clim_collinearity_test))
+    noncollinear_predictors <- stack(subset(predictors, collinearity_test))
     
 
 # 6. Model fitting ------
@@ -183,7 +265,7 @@ myspecies <- c("Nestotus macleanii")
 
 
 # 6.3 Modeling (using Maxent in ENMeval) basic parameters
-  model_species <- ENMeval::ENMevaluate(occs = occs, envs = clim_tiles_merge_agg , bg = bg_points, 
+  model_species <- ENMeval::ENMevaluate(occs = occs, envs = noncollinear_predictors , bg = bg_points, 
                                         algorithm = 'maxent.jar',
                                         partitions = 'block',
                                         tune.args = list(fc = "L", rm = 1),
@@ -195,8 +277,7 @@ myspecies <- c("Nestotus macleanii")
 # 7. Predictions -------
 
   model_species_prediction <- eval.predictions(model_species)
-
-
+  
 # 8.  Model uncertainty ---------
 
 # let's run 10 models and calculate the coefficient of variance (the only think that will change is background points)
@@ -231,12 +312,5 @@ myspecies <- c("Nestotus macleanii")
   model_species_prediction_p <- projectRaster(model_species_prediction, crs = aeac, res = 1000, method = "bilinear")
   uncertainty_p <- projectRaster(uncertainty, crs = aeac, res = 10000, method = "bilinear")
 
-# 10. Plots
-  
-plot(model_species_prediction_p)
-plot(box_extent_analysis_bf$geometry, add=T)
-
-plot(uncertainty_p)
-plot(box_extent_analysis_bf$geometry, add=T)
 #############END PIPELINE ####################################
 
